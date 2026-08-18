6f9061cbe0c6db7e568804a6e3e486eba868494c
30harmain2005-cmyk <30harmain2005@gmail.com>
2026-08-18 21:56:45 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-let url = "http://universities.hipolabs.com/search?name=";
+let url = "https://raw.githubusercontent.com/Hipo/university-domains-list/master/world_universities_and_domains.json";
 let btn = document.querySelector("button");
 let list = document.querySelector(".list");
 let h1 = document.querySelector("h1");
@@ -6,24 +6,36 @@ let h1 = document.querySelector("h1");
 
 async function getCollege(country) {
     try {
-        let res = await axios.get(url+country);
-        return res.data;
+        let res = await axios.get(url);
+        let colleges = res.data;
+        let result = colleges.filter(col =>
+            col.country.toLowerCase() === country.toLowerCase()
+        );
+
+        return result;
     }
     catch(e) {
         console.log("Error -", e);
-        return "No Colleges"
+        return [];
     }
 }
 
 btn.addEventListener("click", async () => {
-    let country = document.querySelector("input").value;
+    let country = document.querySelector("input").value.trim();
     let colArr = await getCollege(country);
     display(colArr, country);
 });
 
 function display(colArr, country) {
     list.innerText = "";
-    for(col of colArr) {
+    
+    if(colArr.length === 0) {
+        let li = document.createElement("li");
+        li.innerText = `No Colleges in ${country}`;
+        list.appendChild(li);
+
+    }
+    for(let col of colArr) {
         let li = document.createElement("li");
         h1.innerText = `Colleges in ${country}`
         li.innerText = col.name;
